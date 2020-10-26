37558e9eafcf82ecb5e328b72a535b1fd8a7c59d
YaaqovShkifati <70407221+YaaqovShkifati@users.noreply.github.com>
2020-10-26 00:41:45 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -7,23 +7,35 @@ package vipclients;
 
 import java.io.IOException;
 
+/**
+ * The LinkList class.
+ */
 public class LinkedList {
 
     String data;
     LinkedList next;
    
-    
+    /**
+     * No argument constructor.
+     */
     public LinkedList(){
         
     }
     
-
+    /**
+     * The LinkedList constructor initializes the data and next pointer.
+     * @param object a String.
+     * @throws IOException  
+     */
     public LinkedList(String object) throws IOException {
         next = null;
         data = object;
     }
     
-
+    /**
+     * The displayLink method returns the data.
+     * @return data.
+     */
     public String displayLink() {
         return data;
     }   
