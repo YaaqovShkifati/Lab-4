209b9b484f3ea75c31a637b1a85fda5dc2dfdfbf
YaaqovShkifati <70407221+YaaqovShkifati@users.noreply.github.com>
2020-10-26 00:10:31 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+@author Yaaqov Shkifati
+@since 10.26.2020
+@verison 1.0
+*/
 package vipclients;
 
 import java.io.IOException;
